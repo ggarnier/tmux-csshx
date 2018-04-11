c097c40cd514e13f711aa0e20810f79efa2a0792
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-04-11 15:27:26 -0300

    Iterate pane splitting per address

--- csshx.sh
+++ csshx.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+addresses="1.2.3.4 5.6.7.8 9.10.11.12"
+
 tmux new-window -n "ssh"
-tmux split-window -v
-tmux send-keys "echo ssh 1" C-m
-tmux select-pane -t 1
-tmux send-keys "echo ssh 2" C-m
+
+i=0
+for addr in `echo $addresses`; do
+    if [ "$i" -gt 0 ]; then
+        tmux split-window -v
+    else
+        tmux select-pane -t "$i"
+    fi
+    tmux send-keys "echo ssh $addr" C-m
+    i=$((i+1))
+done
+
 tmux setw synchronize-panes
