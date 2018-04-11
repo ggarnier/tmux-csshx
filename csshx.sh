267aa10ebdf20b4cbf2b0d58c166e7ee25c0112a
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-04-11 15:29:40 -0300

    Toggle split direction

--- csshx.sh
+++ csshx.sh
@@ -5,9 +5,15 @@ addresses="1.2.3.4 5.6.7.8 9.10.11.12"
 tmux new-window -n "ssh"
 
 i=0
+splitdirection="v"
 for addr in `echo $addresses`; do
     if [ "$i" -gt 0 ]; then
-        tmux split-window -v
+        tmux split-window -"$splitdirection"
+        if [ "$splitdirection" == "v" ]; then
+            splitdirection="h"
+        else
+            splitdirection="v"
+        fi
     else
         tmux select-pane -t "$i"
     fi
