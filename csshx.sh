126c19c45c2833f324459816cf5f1881df914579
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-04-12 13:25:18 -0300

    Fix window splitting

--- csshx.sh
+++ csshx.sh
@@ -5,23 +5,13 @@ addresses="1.2.3.4 5.6.7.8 9.10.11.12"
 tmux new-window -n "ssh"
 
 i=0
-splitdirection="v"
 for addr in `echo $addresses`; do
     if [ "$i" -gt 0 ]; then
-        tmux split-window -"$splitdirection" 2>/dev/null
-        if [ "$?" -ne 0 ]; then
-            tmux select-layout tiled
-            tmux split-window -"$splitdirection"
-        fi
-        if [ "$splitdirection" == "v" ]; then
-            splitdirection="h"
-        else
-            splitdirection="v"
-        fi
+        tmux split-window
+        tmux select-layout tiled
     fi
     tmux send-keys "ssh $addr" C-m
     i=$((i+1))
 done
 
-tmux select-layout tiled
 tmux setw synchronize-panes
