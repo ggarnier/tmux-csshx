60275dcfc212c11e193023fe56f68674f0e89917
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-04-13 13:34:48 -0300

    Add default login argument

--- csshx.sh
+++ csshx.sh
@@ -1,32 +1,41 @@
 #!/bin/sh
 
-addresses="$@"
+addresses=""
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -l) login="$2"; shift 2;;
 
-num_addresses=`echo "$addresses" | wc -w`
+    --login=*) login="${1#*=}"; shift 1;;
+    --login) echo "$1 requires an argument" >&2; exit 1;;
+
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    *) addresses=$(echo "$addresses $1"); shift 1;;
+  esac
+done
+
+num_addresses=$(echo "$addresses" | wc -w)
 if [ "$num_addresses" -eq 0 ]; then
     echo "Need at least one hostname"
     exit 2
 fi
 
 ssh_command () {
-    login="user"
     addr="$1"
-    if [[ ! "$addr" =~ "@" ]]; then
-        addr=`echo $login@$addr`
+    if [[ "$login" != "" && ! "$addr" =~ "@" ]]; then
+        addr=$(echo $login@$addr)
     fi
-    echo "echo ssh $addr"
+    echo "ssh $addr"
 }
 
 tmux new-window -n "ssh"
 
 i=0
-for addr in `echo "$addresses"`; do
+for addr in $(echo "$addresses"); do
     if [ "$i" -gt 0 ]; then
         tmux split-window
         tmux select-layout tiled
     fi
-    cmd=`ssh_command "$addr"`
-    tmux send-keys "$cmd" C-m
+    tmux send-keys "$(ssh_command $addr)" C-m
     i=$((i+1))
 done
 
