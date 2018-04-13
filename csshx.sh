9b04ba9f289534216560e3abe2f9a0560553c6c5
Guilherme Garnier <guilherme.garnier@gmail.com>
2018-04-13 12:42:24 -0300

    Set default user when omitted

--- csshx.sh
+++ csshx.sh
@@ -7,6 +7,16 @@ if [ "$num_addresses" -eq 0 ]; then
     echo "Need at least one hostname"
     exit 2
 fi
+
+ssh_command () {
+    login="user"
+    addr="$1"
+    if [[ ! "$addr" =~ "@" ]]; then
+        addr=`echo $login@$addr`
+    fi
+    echo "echo ssh $addr"
+}
+
 tmux new-window -n "ssh"
 
 i=0
@@ -15,7 +25,8 @@ for addr in `echo "$addresses"`; do
         tmux split-window
         tmux select-layout tiled
     fi
-    tmux send-keys "ssh $addr" C-m
+    cmd=`ssh_command "$addr"`
+    tmux send-keys "$cmd" C-m
     i=$((i+1))
 done
 
